070c37fd86805defe2a4fcde0d841df4a18c3bf3
Julia Mosendz <juliadibrova1@gmail.com>
2025-04-08 22:02:01 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
-import './App.css'
+import "./App.css";
 import { getImages } from "./services/service-api";
-import {SearchForm} from "./components/SearchForm/SearchForm"
+import { SearchForm } from "./components/SearchForm/SearchForm";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState("");
+  useEffect(() => {
+    if (searchQuery === "") {
+      return
+    }
+    async function uploadImages() {
+      const response = await getImages(searchQuery, page);
+      console.log(response);
+      if (response.total_pages === 0) {
+        console.log("Images are not found")
+        return
+      }
+
+      setImages((prevImages) => {
+        return [...prevImages, ...response.results]
+      })
+    }
+    uploadImages()
+  }, [searchQuery, page]);
+
+
+  const handleSearch = (query) => {
+    setImages([])
+    setPage(1)
+    setSearchQuery(query)
+  };
   // getImages()
-  return(
+  return (
     <>
-    <SearchForm/>
+      <SearchForm handleSearch={handleSearch} />
+      {images.length>0 && <p>Show results</p>}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,19 +1,26 @@
-import css from "./SearchForm.module.css"
+import css from "./SearchForm.module.css";
 
-export function SearchForm() {
-    return (
-      <header className={css.header}>
-        <div className={css.container}>
-          <form className={css.form}>
-            <input
-              className={css.input}
-              type="search"
-              name="search"
-              placeholder="Search images and photos"
-            />
-            <button className={css.btn}>Search</button>
-          </form>
-        </div>
-      </header>
-    );
+export function SearchForm(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const query = form.elements.search.value;
+    props.handleSearch(query);
+  };
+
+  return (
+    <header className={css.header}>
+      <div className={css.container}>
+        <form className={css.form} onSubmit={handleSubmit}>
+          <input
+            className={css.input}
+            type="search"
+            name="search"
+            placeholder="Search images and photos"
+          />
+          <button className={css.btn}>Search</button>
+        </form>
+      </div>
+    </header>
+  );
 }
